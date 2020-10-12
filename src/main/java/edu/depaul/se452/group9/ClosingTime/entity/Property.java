6e4d40ffdbee1f9da136a40be090372a81c7ea06
basa <d.basaraba@gmail.com>
2020-10-12 07:38:30 -0500

    updated to use lombok and auto generated mongoId

--- src/main/java/edu/depaul/se452/group9/ClosingTime/entity/Property.java
+++ src/main/java/edu/depaul/se452/group9/ClosingTime/entity/Property.java
@@ -1,35 +1,22 @@
 package edu.depaul.se452.group9.closingtime.entity;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import lombok.Data;
+import lombok.Getter;
+
 
 @Document(collection = "Property")
+@Data
 public class Property {
 
-    @Id
-    private Integer id;
+    @MongoId
+    @Getter private String id;
 
     private String state;
     private String city;
     private String streetAddress;
     private String description;
-
-    public Integer getId() { return id; }
-
-    public String getState() { return state; }
-
-    public void setState(String state) { this.state = state; }
-
-    public String getCity() { return city; }
-
-    public void setCity(String city) { this.city = city; }
-
-    public String getStreetAddress() { return streetAddress; }
-
-    public void setStreetAddress(String streetAddress) { this.streetAddress = streetAddress; }
-
-    public String getDescription() { return description; }
-
-    public void setDescription(String description) { this.description = description; }
 
 }
