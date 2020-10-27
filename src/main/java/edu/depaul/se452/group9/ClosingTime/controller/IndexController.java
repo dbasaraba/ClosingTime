750316adfb5485bcbd1c6ceccf1e3c1326bd6278
pbuko <pbukowski456@gmail.com>
2020-10-27 18:14:27 -0500

    added chat

--- src/main/java/edu/depaul/se452/group9/ClosingTime/controller/IndexController.java
+++ src/main/java/edu/depaul/se452/group9/ClosingTime/controller/IndexController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
+
+
     String index(){
         return "index";
     }
